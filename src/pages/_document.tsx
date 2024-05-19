392c3c772e85fbe363c05d721e86fdca84d4b830
terisuke <terisuke1115@gmail.com>
2024-05-19 12:31:38 +0900

    add bg-c

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body style={{ backgroundImage: `url(${buildUrl("/bg-c.png")})` }}>
+      <body style={{ backgroundImage: `url(${buildUrl("/bg-c-cyber.png")})` }}>
         <Main />
         <NextScript />
       </body>
