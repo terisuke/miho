f27cdc53211d3d89ee9de5b7c164e9f237cd2fc9
terisuke <terisuke1115@gmail.com>
2024-07-04 12:49:44 +0900

    Update .gitignore to exclude .firebase directory

--- src/components/githubLink.tsx
+++ src/components/githubLink.tsx
@@ -6,18 +6,18 @@ export const GitHubLink = () => {
     <div className="absolute right-0 z-10 m-24">
       <a
         draggable={false}
-        href="https://github.com/tegnike/aituber-kit"
+        href="https://forms.gle/HMMJy6mpJRiAuejZ9"
         rel="noopener noreferrer"
         target="_blank"
       >
         <div className="p-8 rounded-16 bg-[#1F2328] hover:bg-[#33383E] active:bg-[565A60] flex">
           <Image
-            alt="GitHub Repository Link"
+            alt="Questionnaire"
             height={24}
             width={24}
-            src={buildUrl("/github-mark-white.svg")}
+            src={buildUrl("/アンケートシートの無料アイコン.svg")}
           />
-          <div className="mx-4 text-white font-bold">Fork me</div>
+          <div className="mx-4 text-white font-bold">ユーザーアンケート</div>
         </div>
       </a>
     </div>
